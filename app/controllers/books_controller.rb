df75f35bfe7af40f6cefdd5e7e7d1ca7ddc3da40
yk-nakamura <yu.s2pg.ki@gmail.com>
2014-09-16 14:10:16 +0900

    Modify dummy data

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
 		
-		dummy1 = {book: {title: "書籍１", author_name: "著者１"}, lending: {lending_date: Date.today, lending_period: 7, lending_user_name: "利用者Ａ"}}
+		dummy1 = {book: {title: "書籍１", author_name: "著者１"}, lending: {date: Date.today, period: 7, user_name: "利用者Ａ"}}
     dummy2 = {book: {title: "書籍２", author_name: "著者２"}, lending: nil}
 		dummy_data = [dummy1, dummy2]		
 
