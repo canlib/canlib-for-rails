b7c372e3de595d8cdced07a469a67f50a6012f2d
yk-nakamura <yuki.nakamura@jrits.ricoh.co.jp>
2014-10-10 16:11:41 +0900

    Improve dialog by addition book data #12

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
 		end
 	end
 	
+	def radio_add_params(book)
+		lending_params = book.lending.present? ? " lending_id=#{book.lending.id} lending_user='#{book.lending.user_name}'" : "\s"
+		raw "<input type='radio' name='book_select' book_id=#{book.id} book_title='#{book.title}' book_author='#{book.author_name}'" + lending_params + "></input>"
+	end
+
 end
